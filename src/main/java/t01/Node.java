feb12c1c4ebe7e926c9020d2df0a288c61d0e7bd
ferventdev <alex.4everyours@gmail.com>
2017-06-24 17:32:22 +0300

    getStepCount method created

--- src/main/java/t01/Node.java
+++ src/main/java/t01/Node.java
@@ -1,7 +1,5 @@
 package t01;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
@@ -26,12 +24,55 @@ public class Node {
     public Node addOrGet(int newValue) {
         if (newValue == value) return this;
 
-        if (newValue < value)
-            if (left == null) return left = new Node(newValue,this);
+        if (newValue < value) {
+            if (left == null) return left = new Node(newValue, this);
             else return left.addOrGet(newValue);
+        }
 
         // newValue > value
         if (right == null) return right = new Node(newValue,this);
         else return right.addOrGet(newValue);
     }
+
+    public int getStepCount(int anotherValue) {
+        return getStepCountHelper(anotherValue, 0, false,false);
+    }
+
+    private int getStepCountHelper(int anotherValue, int count, boolean leftWatched, boolean rightWatched) {
+        if (anotherValue == value) return count;
+
+        int stepCount = -1;
+        if (left != null && anotherValue < value && !leftWatched)
+            stepCount = left.getCountWithinSubtree(anotherValue, count + 1);
+        if (stepCount != -1) return stepCount;
+
+        if (right != null && anotherValue > value && !rightWatched)
+            stepCount = right.getCountWithinSubtree(anotherValue, count + 1);
+        if (stepCount != -1 || parent == null) return stepCount;
+
+        // now parent != null
+        if (this == parent.left) {
+            leftWatched = true;
+            rightWatched = false;
+        } else if (this == parent.right) {
+            rightWatched = true;
+            leftWatched = false;
+        } else throw new AssertionError("This node must be either left or right child of the parent!");
+
+        return parent.getStepCountHelper(anotherValue, count + 1, leftWatched, rightWatched);
+    }
+
+    private int getCountWithinSubtree(int anotherValue, int count) {
+        if (anotherValue == value) return count;
+
+        if (anotherValue < value) {
+            if (left != null) return left.getCountWithinSubtree(anotherValue, count + 1);
+            else return -1;
+        }
+
+        // anotherValue > value
+        if (right != null) return right.getCountWithinSubtree(anotherValue, count + 1);
+        else return -1;
+    }
+
 }
